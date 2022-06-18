0e362a8135130646a2b58db3dc8751641816a256
Panya Sukphranee <psukphranee@gmail.com>
2022-06-18 16:13:14 -0700

    buttons for ML Project sections

--- src/Components/Projects/index.jsx
+++ src/Components/Projects/index.jsx
@@ -6,11 +6,19 @@ const Projects = (props) => {
     let x = [1,2,3,4,5,6,7,8,9,10];
 
     return(
-        <div className='projects' id="projects">
+        <div className='projects-section' id="projects">
             <h1>Machine Learning Projects</h1>
-            <div className='project-content'>
-                <div className="project-category">
-                </div> 
+            <div className='project-container'>
+                <div className="project-categories">
+                    <ul>
+                        <li>MATLAB</li>
+                        <li>Sci-kit Learn</li>
+                        <li>Tensorflow</li>
+                    </ul>
+                </div>
+                <div className='project-items'>
+                    test-test-testicle
+                </div>
                 {/* { 
                     x.map(
                         (currentValue, index) => (
